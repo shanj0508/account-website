aea3ce3b457b93ef356d22c6643ca6e4c13bea42
shanj <605284854@qq.com>
2021-02-21 21:41:27 +0800

    deploy

---
+++ precache-manifest.914cd09343a4fa915d6f18abec5da972.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15eb3fa2be98500725ec",
+    "url": "/account-website/css/app.061ed27a.css"
+  },
+  {
+    "revision": "294bcb45c443f6158290b1e15aadce9a",
+    "url": "/account-website/index.html"
+  },
+  {
+    "revision": "15eb3fa2be98500725ec",
+    "url": "/account-website/js/app.b5602898.js"
+  },
+  {
+    "revision": "35e33cf7acee695467da",
+    "url": "/account-website/js/chunk-vendors.74d2a24e.js"
+  },
+  {
+    "revision": "fdf3677cc3d5dc6c53a94bdffe9f4094",
+    "url": "/account-website/manifest.json"
+  },
+  {
+    "revision": "c13883453cdbe5cc2842d795225284d6",
+    "url": "/account-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-website/robots.txt"
+  }
+]);
